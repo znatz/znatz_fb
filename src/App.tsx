b4cf1053db643d545e24cb31b731d10ac0ca3731
znatz <z.nat.cn@gmail.com>
2020-11-13 01:08:23 +0900

    show user list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState, useEffect } from 'react';
-import { firestore } from 'firebase';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+// firebase functions
+import firebase from 'firebase';
+import fireStore from './findex'
+
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [users, setUsers] = useState<firestore.DocumentData[]>([]);
+  const [users, setUsers] = useState<firebase.firestore.DocumentData[]>([]);
+  const [myAccount, setMyAccount] = useState();
+
   useEffect(() => {
     const searchUsers = async() => {
-      // Firestoreのコレクションを指定してデータ取得。今回は全量を検索
-      const res = await firestore.collection('users').get();
+      // Firestoreのコレクションを指定してデータ取得
+      const res = await firebase.firestore().collection('users').get();
       if (res.empty) return [];
-      const userList: firestore.DocumentData[] = [];
+      const userList: firebase.firestore.DocumentData[] = [];
       // DocumentData型にはmapメソッドが定義されていないため、forEachのループでデータを加工
       res.forEach(doc => {
           userList.push(doc.data());
       })
       setUsers(userList);
     }
-
     searchUsers();
-    setLoading(false);
+
 }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-        {users.map(data => (
-          <p>{data.ip}</p>
-        ))}
-        </div>
+        {loading ? (
+                  <p>
+                  LOADING.....
+                </p> 
+        ) : !myAccount ? (
+          <p>
+            ログインが必要です
+          </p>
+        ) : 
+          users.map((user, index) => {
+            return <p key={index}> {user.ip}</p>
+          })
+        }
       </header>
     </div>
   );
---
+++ src/findex.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+
+const env = process.env;
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCkVm5qa0KBl2yJd0gPk0_-9Cp-5-OkYdw",
+    authDomain: "znatz-72cf7.firebaseapp.com",
+    databaseURL: "https://znatz-72cf7.firebaseio.com",
+    projectId: "znatz-72cf7",
+    storageBucket: "znatz-72cf7.appspot.com",
+    messagingSenderId: "635513117633",
+    appId: "1:635513117633:web:b3ccd19d9454c4c482ad40",
+    measurementId: "G-RD0JQN4K4S"
+};
+
+// Firebaseを紐付け、初期化
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Firestoreのインスタンス作成
+export const firebaseStore = firebaseApp.firestore();
+
+export default firebase;
