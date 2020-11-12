88d90c09c009615fd89392451db90aaa06d774c0
znatz <z.nat.cn@gmail.com>
2020-11-13 01:15:30 +0900

    No Firebase App '[DEFAULT]' has been created - call Firebase.initializeApp()

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 // firebase functions
-import firebase from 'firebase';
-import fireStore from './findex'
+import firebase from './findex'
 
 
 function App() {
@@ -31,15 +30,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {loading ? (
-                  <p>
-                  LOADING.....
-                </p> 
-        ) : !myAccount ? (
-          <p>
-            ログインが必要です
-          </p>
-        ) : 
+        { 
           users.map((user, index) => {
             return <p key={index}> {user.ip}</p>
           })
